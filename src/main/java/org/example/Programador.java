05bdb660641afe4e5a444bf51a0486fc7b850d1b
Angel_Ferreras <angelferreras13@.icloud.com>
2026-02-28 08:45:35 -0500

    Update logic and method names

--- src/main/java/org/example/Programador.java
+++ src/main/java/org/example/Programador.java
@@ -2,31 +2,50 @@ package org.example;
 import java.util.Scanner;
 
   public  class Programador {
-      String nombre;
-      String lenguajePrincipal;
-      String añosExperiencia;
+      String name;
+      String MainLanguage;
+      String yearsOfExperience;
 
-      //programmer method what it can do
-      //metodo programador lo que puede hacer
-      void presentarse() {
-          System.out.println("Hola, soy " + nombre + ". Domino " + lenguajePrincipal + " y tengo " + añosExperiencia + " años de Experiencia");
+      void evaluateProfile() {
+          // We convert the String to an integer to perform comparisons
+          int years = Integer.parseInt(yearsOfExperience);
+          String rank;
+
+          // Decision Logic (IF - ELSE Structure)
+          if (years < 2) {
+              rank = "Junior (You are just starting your journey!)";
+          } else if (years >= 2 && years < 5) {
+              rank = "mid-level (You have a solid traack record";
+          } else {
+              rank = "Senior (You are an expert)";
+          }
+
+          System.out.println("\n--- ANALYSIS RESULT ---\n");
+          System.out.println("Developer: " + name);
+          System.out.println("Main Language: " + MainLanguage);
+          System.out.println("Assigned Rank: " + rank);
+          System.out.println("\n--- ANALYSIS COMPLETE---");
       }
-    public static void  main(String[] args) {
-        Scanner lector = new Scanner(System.in);
+      public static void  main(String[] args) {
+          Scanner lector = new Scanner(System.in);
 
-        Programador nuevoTalento = new Programador();
+          Programador nuevoTalento = new Programador();
 
-        System.out.println("--- Registro de Desarrollador ---");
-        System.out.println("Introduce tu nombre: ");
-        nuevoTalento.nombre = lector .nextLine();
+          System.out.println("--- Registro de Desarrollador ---");
+          System.out.println("Introduce tu nombre: ");
+          nuevoTalento.name = lector .nextLine();
 
-        System.out.println("Que lenguaje estas aprendiendo?: ");
-        nuevoTalento.lenguajePrincipal = lector .nextLine();
+          System.out.println("Que lenguaje estas aprendiendo?: ");
+          nuevoTalento.MainLanguage = lector .nextLine();
 
-        System.out.println("Cuantos años tienes de experiencia?");
-        nuevoTalento.añosExperiencia = lector .nextLine();
+          System.out.println("Cuantos años tienes de experiencia?");
+          nuevoTalento.yearsOfExperience = lector .nextLine();
+//Pruebaaa
+          nuevoTalento.evaluateProfile();
+          System.out.println("¡Configuracion guardada para " + nuevoTalento.name + "!");
 
-        nuevoTalento.presentarse();
-        System.out.println("¡Configuracion guardada para " + nuevoTalento.nombre + "!");
-    }
+
+      }
   }
+
+
